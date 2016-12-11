8209e091022df5f5bc4a5f15d569e1b0e182a432
Dunbarry <RyanADunbar@gmail.com>
2016-12-11 16:45:59 -0700

    :bug: Crate distribution bug fix.

--- Scripts/CrateDistribution.cs
+++ Scripts/CrateDistribution.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class CrateDistribution : MonoBehaviour {
 
-
+	string[] gases = new string[] {"N", "Ar", "O", "P", "K", "He"};
 	public GameObject Crates;
 	public GameObject CratesContain;
+//	string loadCrate;
+	int loadControl;
+	int count;
 
 	private int xCoord;
 	private int zCoord;
@@ -22,13 +25,24 @@ public class CrateDistribution : MonoBehaviour {
 
 	void placeCrates () {
 		for (int c = 0; c <= 11; c++) {
-			xCoord = Random.Range (-15, 15);
-			zCoord = Random.Range (-15, 15);
+			count = c;
+			if(count >= 6){
+				loadControl = c - 6;
+			}
+			else {
+				loadControl = c;
+			}
+
+			xCoord = Random.Range (-20, 20);
+			zCoord = Random.Range (-20, 20);
 			var newCrate = Instantiate (Crates, new Vector3 (xCoord, 0, zCoord), Quaternion.identity) as GameObject;
 			newCrate.transform.SetParent (CratesContain.transform, false);
 //			newCrate.transform.localPosition = Crates.transform.localPosition;
 //			newCrate.transform.localScale = Crates.transform.localScale;
 			newCrate.transform.localRotation = Crates.transform.localRotation;
+
+			CrateController crateControl = newCrate.GetComponent<CrateController>();
+			crateControl.crateContents = gases[loadControl];
 		}
 	}
 }
