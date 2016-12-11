6261bb74e649c6819e6619b9d0f17ffe115d70f7
Dunbarry <RyanADunbar@gmail.com>
2016-12-10 18:47:10 -0700

    :package: Hot, hot drone on crate action. Bow chick a'wo-WOW

--- Scripts/CrateController.cs
+++ Scripts/CrateController.cs
@@ -5,26 +5,26 @@ using UnityEngine.UI;
 public class CrateController: MonoBehaviour {
 
 	public GameObject CrateContentsText;
-//	public GameObject CrateContentsText;
+	public GameObject CrateActionText;
+	private GameObject opener;
 
 	public string crateContents;
 //	var arr3 = new int[] { 1, 2, 3 };
 	string[] gases = new string[] {"C", "N", "O", "P", "S", "Se"};
 
+	private string crateAction;
+
 	// Use this for initialization
 	void Start () {
 		crateContents = gases[Random.Range (0, 5)];
-	}
-
-	// Update is called once per frame
-	void Update () {
-
+		crateAction = "Q";
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		Debug.Log ("Collider Entered");
 		CrateCommand (other);
+		CrateApproach (other);
 	}
 
 	void OnTriggerStay(Collider other){
@@ -33,11 +33,27 @@ public class CrateController: MonoBehaviour {
 
 	void CrateCommand(Collider other)
 	{
-//		if (other.gameObject.CompareTag ("Player")) {
-//			GameObject opener = Instantiate (
-		if(Input.GetKeyDown("e")){
-			Debug.Log("Opening the crate!");
+		if (Input.GetKeyDown ("q")) {
+			Debug.Log ("Opening the crate!");
 			InitCrateContentsText (crateContents);
+		}
+	}
+
+	void CrateApproach(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			GameObject opener = Instantiate (CrateActionText) as GameObject;
+			RectTransform openerRect = opener.GetComponent<RectTransform> ();
+			opener.transform.SetParent (transform.FindChild ("CrateCanvas"));
+			openerRect.transform.localPosition = CrateContentsText.transform.localPosition;
+			openerRect.transform.localScale = CrateContentsText.transform.localScale;
+			openerRect.transform.localRotation = CrateContentsText.transform.localRotation;
+
+			opener.GetComponent<Text> ().text = crateAction;
+			opener.GetComponent<Animator> ().SetTrigger ("OnTriggerEnter");
+			Destroy (opener.gameObject, 2);
+		} else if (Input.GetKeyDown ("q")) {
+			Destroy (opener.gameObject);
 		}
 	}
 
@@ -46,9 +62,9 @@ public class CrateController: MonoBehaviour {
 		GameObject temp = Instantiate (CrateContentsText) as GameObject;
 		RectTransform tempRect = temp.GetComponent<RectTransform> ();
 		temp.transform.SetParent(transform.FindChild("CrateCanvas"));
-		tempRect.transform.localPosition = CrateContentsText.transform.localPosition;
-		tempRect.transform.localScale = CrateContentsText.transform.localScale;
-		tempRect.transform.localRotation = CrateContentsText.transform.localRotation;
+		tempRect.transform.localPosition = CrateActionText.transform.localPosition;
+		tempRect.transform.localScale = CrateActionText.transform.localScale;
+		tempRect.transform.localRotation = CrateActionText.transform.localRotation;
 
 		temp.GetComponent<Text>().text = text;
 		temp.GetComponent<Animator> ().SetTrigger ("CrateCommand");
